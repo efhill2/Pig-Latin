136c2c3554d87b41a9dda23633cf799c15afbf70
Eugene Hill <eugenehill@quickenloans.com>
2020-07-22 08:33:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace PigLatin
+{
+    class Program
+    {
+        public static void CheckVowels(string word)
+        {
+
+        }
+
+        public static void CheckConsonant(string word)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', };
+
+            Console.WriteLine("Welcome to the Pig Latin translator!");
+
+            //do
+            //{
+
+                Console.WriteLine("Please enter a word you want translated");
+
+                string userWord = Console.ReadLine().ToLower();
+
+                foreach (char vowel in vowels)
+                {
+                    if (userWord[0].Equals(vowel))
+                    {
+                        Console.WriteLine($"{userWord}way");
+                    }
+                    else if (!userWord[0].Equals(vowel))
+                    {
+                        int index = userWord.IndexOfAny(vowels);
+
+                        string frontConsonants = userWord.Substring(0, index);
+                        string translation = userWord.Substring(index);
+
+                        Console.WriteLine($"{translation}{frontConsonants}ay");
+                        break;
+
+                    }
+                }
+            //}
+            
+
+        }
+    }
+}
